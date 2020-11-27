5a540f86b23012d6fa514f0f8fd469468e6f82e3
Muhammad Moinuddin <54661456+Muhammed-Moinuddin@users.noreply.github.com>
2020-11-27 10:40:52 +0500

    Add files via upload

---
+++ codeday1.py
@@ -0,0 +1,49 @@
+print("hello,world!")
+name = "mark"
+name = "ace"
+print(name)
+thanx = "thanks for your input!"
+print(thanx)
+weight = 150
+sum = weight + 150
+print(sum)
+print(13*4)
+print(12/5)
+print(12%5)
+print(10%3)
+print(3%10)
+print(3/10)
+age = 50
+age += 18
+print(age)
+age *= 2
+print(age)
+greeting = "Hello"
+address = "World"
+separators = ", "
+punc = "!"
+whole_greeting = greeting + separators + address + punc
+print(whole_greeting) #concatenation
+whole_greeting2 = "Hello" + separators + "World!"
+print(whole_greeting2) 
+print("Hello" + separators + "World!")
+print("The sum of 2 + 2 is",2+2)
+print("The sum of 3 + 3 is " + "6")
+species = "cat"
+if species == "cat":
+    print("Yep,Its a cat")
+if 2+2 == 4:
+    print("Everything makes sense")   
+if species == "cat":
+    print("So far so good")
+    print("Congratulations")
+    print("All done")
+sentence = "is my name and my age is"     
+full_name = "Mark is my name and my age is 20"      
+if full_name == "Mark " + sentence + " 20" :
+    print("You've done a right thing")
+labour_cost = 120
+material_cost = 180
+total_cost = 300
+if total_cost == labour_cost + 180:
+    print("You've done right calculations")    
